ebb3bf1f26571835400b534fcfe0facca2030682
StrandsMedia <strands.media@gmail.com>
2018-08-30 09:09:04 +0400

    30.08.18 1

---
+++ src/app/common/modules/snackbar/snackbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SnackbarComponent } from '../../snackbar/snackbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SnackbarComponent]
+})
+export class SnackbarModule { }
