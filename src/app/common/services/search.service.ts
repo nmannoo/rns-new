d0f41cf93887af57593b09cd49c0c032c63d25aa
StrandsMedia <strands.media@gmail.com>
2018-09-11 10:56:14 +0400

    11.09.18 3

--- src/app/common/services/search.service.ts
+++ src/app/common/services/search.service.ts
@@ -3,27 +3,39 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import * as firebase from 'firebase';
 
-const algoliasearch = require('algoliasearch/dist/algoliasearch.js');
+import { PlatformService } from '../services/platform.service';
 
 import { environment } from '../../../environments/environment';
 
-import { Observable, from } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  // Create an instance of Algolia Search
-  private client = algoliasearch(environment.algolia.appId, environment.algolia.apiKey);
-
   // Observable to return from the service function
   algosearch: Observable<any>;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(
+    private afs: AngularFirestore,
+    private platform: PlatformService
+  ) { }
 
   searchQuery(query: string): Observable<any> {
-    const index = this.client.initIndex('dev_PRODS');
-    this.algosearch = from(index.search(query));
-    return this.algosearch;
+    if (this.platform.platformCheck) {
+      const algoliasearch = require('algoliasearch/dist/algoliasearch.js');
+
+      // Create an instance of Algolia Search
+      const client = algoliasearch(environment.algolia.appId, environment.algolia.apiKey);
+
+      const index = client.initIndex('dev_PRODS');
+      this.algosearch = from(index.search(query)).pipe(
+        debounceTime(2000)
+      );
+      return this.algosearch;
+    } else {
+      return of(null);
+    }
   }
 }
